e2a66f52632999f9c035d2cd06d77dddf274c79c
Moritz Platt <moritz.platt@r3.com>
2018-08-20 16:01:47 +0100

    removed helpers that are unnecessary for an internal mock network

--- bn-apps/ledger-sync/ledger-sync-service/src/test/kotlin/net/corda/businessnetworks/ledgersync/LedgerSyncWhitelist.kt
+++
@@ -1,8 +0,0 @@
-package net.corda.businessnetworks.ledgersync
-
-import net.corda.core.serialization.SerializationWhitelist
-import java.util.concurrent.ConcurrentHashMap
-
-class LedgerSyncWhitelist : SerializationWhitelist {
-    override val whitelist: List<Class<*>> = listOf(ConcurrentHashMap::class.java)
-}
