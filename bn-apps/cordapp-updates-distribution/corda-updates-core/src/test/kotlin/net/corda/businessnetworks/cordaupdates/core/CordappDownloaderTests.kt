c00072bf5b2f9b8448678312768caefe195d7ec4
Ivan Schasny <ivan.schasny@r3.com>
2018-09-20 12:10:10 +0100

    Added sample test

--- bn-apps/cordapp-updates-distribution/corda-updates-core/src/test/kotlin/net/corda/businessnetworks/cordaupdates/core/CordappDownloaderTests.kt
+++ bn-apps/cordapp-updates-distribution/corda-updates-core/src/test/kotlin/net/corda/businessnetworks/cordaupdates/core/CordappDownloaderTests.kt
@@ -1,3 +1,17 @@
 package net.corda.businessnetworks.cordaupdates.core
 
+import org.junit.Test
+import kotlin.test.assertEquals
 
+class CordappDownloaderTests {
+    companion object {
+        val LOCAL_REPO_PATH = "./TestLocalRepo"
+    }
+
+    @Test
+    fun simpleTest() {
+        val downloader = CordappDownloader("file://${CordappDownloaderTests::class.java.classLoader.getResource("TestRepo").file!!}", LOCAL_REPO_PATH)
+        val versions = downloader.listVersions("net.example:test-artifact:[0,)")
+        assertEquals(versions.versions!!.map { it.toString() }.toSet(), setOf("0.1", "0.5", "1.0", "1.5", "2.0"))
+    }
+}
