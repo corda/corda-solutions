f56d9959364e1aa3bd79a1616c0af6e55b55e382
Ivan Schasny <ivan.schasny@r3.com>
2018-09-28 17:23:58 +0100

    Added implementation for sync-with-the-remote flow

---
+++ bn-apps/cordapp-updates-distribution/corda-updates-app/src/main/kotlin/net/corda/cordaupdates/app/Utils.kt
@@ -0,0 +1,13 @@
+package net.corda.cordaupdates.app
+
+import net.corda.businessnetworks.cordaupdates.core.SyncerConfiguration
+import net.corda.core.node.ServiceHub
+import java.io.File
+
+object Utils {
+    fun syncerConfiguration(serviceHub : ServiceHub) : SyncerConfiguration {
+        val configuration = serviceHub.cordaService(ClientConfiguration::class.java)
+        val syncerConfigPath = configuration.syncerConfig()
+        return if (syncerConfigPath == null) SyncerConfiguration.readFromFile() else SyncerConfiguration.readFromFile(File(syncerConfigPath))
+    }
+}
