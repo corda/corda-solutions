b3e3c3bca4297256c9735f6ebc105df764258d69
Alex Koller <alex.koller@r3.com>
2018-10-01 16:40:22 +0100

    Issuing ticket Volume 1

--- bn-apps/ticketing-service/ticketing-service-cordapp/src/test/kotlin/net/corda/businessnetworks/ticketing/test/IssueTicketTest.kt
+++ bn-apps/ticketing-service/ticketing-service-cordapp/src/test/kotlin/net/corda/businessnetworks/ticketing/test/IssueTicketTest.kt
@@ -1,6 +1,27 @@
 package net.corda.businessnetworks.ticketing.test
 
+import net.corda.businessnetworks.membership.bno.*
+import net.corda.businessnetworks.membership.member.AmendMembershipMetadataFlow
+import net.corda.businessnetworks.membership.member.GetMembershipsFlow
+import net.corda.businessnetworks.membership.member.RequestMembershipFlow
+import net.corda.businessnetworks.membership.bno.service.DatabaseService
+import net.corda.businessnetworks.membership.states.Membership
+import net.corda.businessnetworks.membership.states.MembershipMetadata
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.flows.FlowException
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.identity.Party
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.utilities.getOrThrow
+import net.corda.testing.internal.chooseIdentityAndCert
+import net.corda.testing.node.MockNetwork
+import net.corda.testing.node.MockNetworkNotarySpec
+import net.corda.testing.node.MockNodeParameters
+import net.corda.testing.node.StartedMockNode
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import kotlin.test.fail
 
 class IssueTicketTest : AbstractBaseTest(2) {
 
